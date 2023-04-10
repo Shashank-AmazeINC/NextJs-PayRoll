d1a758a77974327739cae5c1c6d5cf503664e3ef
Gopi-amazeinc <gopi@amazeinc.in>
2023-04-10 16:53:02 +0530

    login page

--- Components/layout.js
+++ Components/layout.js
@@ -1,47 +1,98 @@
-import React, { useEffect } from 'react'
-import Header from './header'
+import React, { useEffect, useState }  from 'react';
+import Header from './header';
+import styles from './layout.module.css';
 import Sidebar from './sidebar'
 import Login from './login';
+import Head from 'next/head';
+import Footer from './footer';
 
-function Layout({ children }) {
 
-    let isLogin = "no";
-
+const  Layout=({ children })=> {
+    const [isLogin, setILogin] = useState("no");
+    const [pageName, setPageName] = useState("Dashboard");
     useEffect(() => {
-        isLogin = sessionStorage.getItem("isLogin");
+        console.log("layout rendered");
+        let login = sessionStorage.getItem("isLogin");
+        if (login && login == "yes") {
+            setILogin("yes");
+        }
+        else {
+            setILogin("no");
+        }
+        // setusernameName(sessionStorage.getItem("userName"));
+        let pn = sessionStorage.getItem("pageName");
+        if (pn) {
+            setPageName(pn);
+        }
+    }, []);
 
-    })
+   const login = () => {
+        setILogin("yes");
+        sessionStorage.setItem("isLogin", "yes");
+        location.href = "/Home/dashboard";
+    }
 
+    const logout = () => {
+        setILogin("no");
+        sessionStorage.clear();
+        sessionStorage.setItem("isLogin", "no");
+        location.href = "/";
+    }
 
-    // if (isLogin == "yes") {
-    return (
-        <div className='row'>
-            <div className='col-lg-12'>
-                <Header></Header>
+    const changePageName = () =>{
+        sessionStorage.setItem("pageName", data);
+        setPageName(data);
+    }
 
-            </div>
-            <div>
+    if (isLogin == "yes") {
+        return (
+            <>
+             <div className={styles.divNegate}>
+                <Head>
+                    <title>Next Js Payrool </title>
+                </Head>
+                <div className='row'>
+                <div className='col-lg-12' style={{ backgroundColor: '#f5f8fb' }}>
+
+                        <Header makelogout={logout} pageName={pageName} ></Header>
+                    </div>
+                    </div>
 
                 <div className='row'>
                     <div className='col-lg-2'>
                         <Sidebar></Sidebar>
                     </div>
-                    <div className='col-lg-10'>
-                        <main>{children}</main>
+                    <div className='col-lg-10' style={{ backgroundColor: '#f5f8fb' }}>
+                        <main pageName={pageName} style={{ height: '90vh', overflowY: 'auto' }}>{children}</main>
+                         <Footer></Footer>
+                    </div>
+                </div>
+                {/* <div className='row'>
+                <div className='col-lg-12' style={{ backgroundColor: '#f5f8fb' }}>
+                       
+                    </div>
+                    </div> */}
+            </div>
+        </>
+        )
+    }
+    else {
+        return (
+            <div className={styles.divNegate}>
+                <Head>
+                    <title style={{color:'white'}}>Next Js Payrool</title>
+                </Head>
+                <div className='row'>
+                    <div className='col-lg-12'>
+                        {/* <h1>login</h1> */}
+                        <Login makelogin={login}></Login>
                     </div>
                 </div>
             </div>
-        </div>
-    )
-    // }
-    // else
-    //     return (
-    //         <div className='row'>
-    //             <div className='col-lg-12'>
-    //                 <Login></Login>
-    //             </div>
-    //         </div>
-    //     )
+            
+        )
+    }
 }
+
 
 export default Layout
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,13 @@ import Layout from '@/Components/layout.js'
 function Index({}) {
   return (
     <Layout>
-        
+        <div className='container-fluid'>
+        <div className='row'>
+          <div className='col-lg-9'>
+
+          </div>
+          </div>
+          </div>
     </Layout>
   )
 }

0182f7c98fce9f27ce259aac776690f6f2acc42a
Naveen <naveen.th@amazeinc.in>
2023-04-10 11:07:49 +0530

    workLocationDashboard- in progress

--- pages/Masters/worklocationmasterdashboard.js
+++ pages/Masters/worklocationmasterdashboard.js
@@ -1,8 +1,24 @@
 import Link from 'next/link'
 import React from 'react'
 import Layout from '@/Components/layout'
+import Styles from '../../styles/table.module.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function WorkLocationMasterDash() {
+
+const [workLocation, setWorkLocationData] = useState([]);
+
+    useEffect(() => {
+        async function getData() {
+          let res = await axios.get(
+            ""
+          );
+          setWorkLocationData();
+        }
+        getData();
+      }, [1]);
+
     return (
         <Layout>
             <div>
@@ -24,6 +40,31 @@ function WorkLocationMasterDash() {
                     <div className='col-lg-2 '>
                         <Link href="/Masters/worklocationmasterform" id='AddButton' className='btn btn-primary fw-bold'>Add New</Link>
                     </div>
+                </div>
+
+                <div className='row '>
+
+                    <table className=' table mt-3 table-striped ' id={Styles.table} >
+                        <thead>
+                            <tr id={Styles.tr}>
+
+                                <th >Loan Type</th>
+                                <th >Description</th>
+                                <th >Action</th>
+                            </tr>
+                        </thead>
+                        <tbody >
+                            <tr>
+
+                                <td>Agriculture Loan</td>
+                                <td>This loan would be available for rearing all types of animals.</td>
+                                <td><span className='form-check form-switch  '>
+                                    <input id={Styles.input} className='form-check-input  ' type="checkbox" /> &nbsp;</span> </td>
+
+                            </tr>
+                           
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </Layout>

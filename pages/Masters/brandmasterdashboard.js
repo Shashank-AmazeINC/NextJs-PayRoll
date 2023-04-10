0d810608d1b2c173dbe2c4dadf402894bb27d070
manjan <manjan@amazeinc.in>
2023-04-10 15:27:13 +0530

    changes

--- pages/Masters/brandmasterdashboard.js
+++ pages/Masters/brandmasterdashboard.js
@@ -1,99 +1,141 @@
-import React from 'react'
-import brandmaster from '../../styles/BrandMasterDashboard.module.css'
-import Link from 'next/link'
-import Layout from '@/Components/layout'
+import React from "react";
+import brandmaster from "../../styles/BrandMasterDashboard.module.css";
+import Link from "next/link";
+import Layout from "@/Components/layout";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function BrandMasterDashboard() {
-    return (
-        <Layout>
-            <div>
-                <div className="container-fluid">
+  const [BrandMaster, setBrandMaster] = useState([]);
 
-                    <div className="row">
-                        <div className="col-md-12">
-                            <div className="row">
-                                <div className="col-lg-6">
-                                    <h3 className="text-primary fs-5 mt-3 fw-bold">Band Master</h3>
-                                </div>
-                            </div>
-                            <br />
-                            <div className={brandmaster.card}>
-                                <div className="row">
-                                    <div className="col-lg-1">
-                                        <p >Filter By</p>
-                                    </div>
-                                    <div className="col-lg-5">
-                                        <input type="text" placeholder="Search" id="term" className="form-control text" />
-                                    </div>
-                                </div>
-                            </div>
-                            <br />
-                            <div className="row">
-                                <div className="col-md-10">
-                                </div>
-                                <div className="col-md-2">
-                                    <Link href="/Masters/brandmasterform"><button className={brandmaster.button} tabindex="0">
-                                        Add New</button></Link>
-                                </div>
-                            </div>
-                            <br />
-                            <div className="alignForm">
-                            </div>
-                            <div className="row">
-                                <div className="col-md-12">
-                                    <div className="container-fluid">
+  const getBrandMaster = async () => {
+    let hostURL = process.env.NEXT_PUBLIC_API_HOST_URL;
+    let res = await axios.get(hostURL + "Master/GetBrandMaster");
+    setBrandMaster(res.data);
+  };
 
-                                        <br />
-                                        <table class="table table-bordered text">
-                                            <thead >
-                                                <tr class="head">
-                                                    <th class="col-md-4">Short Name</th>
-                                                    <th class="col-md-4">Description</th>
-                                                    <th class="col-md-4" style={{ textAlign: "center" }}>Action</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody >
-                                                <tr >
-                                                    <td >Associate Manager</td>
-                                                    <td >associate</td>
-                                                    <td >
-                                                        <button class="edit-btn">Edit</button> <button class="edit-btn">Delete</button>
-                                                    </td>
-                                                </tr>
-                                                <tr >
-                                                    <td >Manager</td>
-                                                    <td >Manager</td>
-                                                    <td >
-                                                        <button class="edit-btn">Edit</button> <button class="edit-btn">Delete</button>
-                                                    </td>
-                                                </tr>
-                                                <tr >
-                                                    <td >Prop Mgt - Field</td>
-                                                    <td >Prop Mgt - Field
-                                                    </td>
-                                                    <td >
-                                                        <button class="edit-btn">Edit</button>
-                                                        <button class="edit-btn">Delete</button>
-                                                    </td></tr>
-                                                <tr >
-                                                    <td >Staff</td>
-                                                    <td >Staff</td>
-                                                    <td >
-                                                        <button class="edit-btn">Edit</button>
-                                                        <button class="edit-btn">Delete</button>
-                                                    </td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  useEffect(() => {
+    getBrandMaster();
+  }, [1]);
 
-                </div><br />
+  const getBandMasterData = (data) => {
+    sessionStorage.setItem("id", data.id);
+    console.log(data.id);
+  };
+
+  const clearFormData = () => {
+    sessionStorage.setItem("id", "");
+  };
+
+  async function DeleteBandMaster(id) {
+    debugger
+    try {
+      let hostURL = process.env.NEXT_PUBLIC_API_HOST_URL;
+      const res = await axios.get(
+        hostURL + `Master/DeleteBrandMaster?id=${id}`
+      );
+      console.log(res.data);
+      alert("Data Deleted Sucessfully");
+      getBrandMaster();
+    } catch (error) {
+      console.error(error);
+      alert("Failed to delete data");
+    }
+  }
+
+  return (
+    <Layout>
+      <div>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="row">
+                <div className="col-lg-6">
+                  <h3 className="text-primary fs-5 mt-3 fw-bold">
+                    Band Master
+                  </h3>
+                </div>
+              </div>
+              <br />
+              <div className={brandmaster.card}>
+                <div className="row">
+                  <div className="col-lg-1">
+                    <p>Filter By</p>
+                  </div>
+                  <div className="col-lg-5">
+                    <input
+                      type="text"
+                      placeholder="Search"
+                      id="term"
+                      className="form-control text"
+                    />
+                  </div>
+                </div>
+              </div>
+              <br />
+              <div className="row">
+                <div className="col-md-10"></div>
+                <div className="col-md-2">
+                  <Link href="/Masters/brandmasterform">
+                    <button
+                      className={brandmaster.button}
+                      tabindex="0"
+                      onClick={clearFormData.bind(this)}
+                    >
+                      Add New
+                    </button>
+                  </Link>
+                </div>
+              </div>
+              <br />
+              <div className="alignForm"></div>
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="container-fluid">
+                    <br />
+                    <table class="table table-bordered text">
+                      <thead>
+                        <tr class="head">
+                          <th class="col-md-4">Short Name</th>
+                          <th class="col-md-4">Description</th>
+                          <th class="col-md-4" style={{ textAlign: "center" }}>
+                            Action
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {BrandMaster.map((data) => {
+                          return (
+                            <tr key={data.id}>
+                              <td>{data.short}</td>
+                              <td>{data.description}</td>
+                              <td>
+                                <Link href="/Masters/brandmasterform">
+                                  <button
+                                    className={brandmaster.button}
+                                    onClick={getBandMasterData.bind(this, data)}
+                                  >
+                                    Edit
+                                  </button>
+                                </Link>
+                                &nbsp;
+                                <button className={brandmaster.button}onClick={() => DeleteBandMaster(data.id)}>Delete{" "}</button>
+                              </td>
+                            </tr>
+                          );
+                        })}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
             </div>
-        </Layout>
-    )
+          </div>
+        </div>
+        <br />
+      </div>
+    </Layout>
+  );
 }
 
-export default BrandMasterDashboard
+export default BrandMasterDashboard;

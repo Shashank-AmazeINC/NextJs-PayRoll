a4ce826237b41a0080f5bfa265d3295e7faa5ca8
Gopi-amazeinc <gopi@amazeinc.in>
2023-04-11 15:29:05 +0530

    pushed chnaged

--- pages/Masters/leavetypeform.js
+++ pages/Masters/leavetypeform.js
@@ -4,84 +4,165 @@ import { useForm } from 'react-hook-form';
 import dynamic from 'next/dynamic';
 import leaveform from '../../styles/LeaveTypeForm.module.css'
 import Layout from '@/Components/layout';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Swal from "sweetalert2";
+
 function LeaveTypeForm() {
 
-    // new Swal({
-    //     title: "Please fill all the Details",
-    //     showCancelButton: true,
-    //     animation: false
-    // }, function (confirmed) {
-    //     alert("Sweet Alert response received: " + confirmed);
-    // });
+    let hostURL = process.env.NEXT_PUBLIC_API_HOST_URL;
+  const [actionType, setActionType] = useState("insert");
 
-    // form validation rules 
+  const { register, handleSubmit, reset, formState } = useForm();
+  const { errors } = formState;
 
-    // get functions to build form with useForm() hook
-    const { register, handleSubmit, reset, formState } = useForm();
-    const { errors } = formState;
+
+  useEffect(() => {
+    const getLeaveTypeMasterList = async () => {
+      debugger;
+      const id = sessionStorage.getItem("id");
+      if (id) {
+        const response = await axios.get(
+          hostURL + "Master/GetLeaveTypeByID?ID=" + id
+        );
+        clearForm(response.data[0]);
+      } else {
+        clearForm();
+      }
+    };
+    getLeaveTypeMasterList();
+  }, [1]);
+
+  const onSubmit = async (data) => {
+    debugger;
+    console.log(data);
+    if (actionType == "insert") {
+      await axios.post(hostURL + "Master/InsertLeaveTypeMaster", data);
+      Swal.fire("SubSectionMaster Inserted succefully!");
+      location.href = "/Masters/leavetypedashboard";
+    } else {
+      let res = await axios.post(hostURL + "Master/UpdateLeaveTypeMaster", data);
+      sessionStorage.removeItem("id");
+      Swal.fire("SubSectionMaster updated succefully!");
+      location.href = "/Masters/leavetypedashboard";
+    }
+  };
+  const clearForm = (existingData = null) => {
+    let etty = {
+      ID: existingData ? existingData.id : "",
+      Short: existingData ? existingData.short : "",
+      Description: existingData ? existingData.description : "",
+    };
+    reset(etty);
+    setActionType(existingData ? "update" : "insert");
+  };
+  const customStyles = {
+    errorMsg: {
+      fontSize: "12px",
+      fontWeight: "500",
+      color: "red",
+    },
+    inputLabel: {
+      fontSize: "16px",
+    },
+  };
 
 
 
     return (
         <Layout>
-            <div className="container-fluid">
-                <div className={leaveform.row}>
-                    <div className="col-lg-2">
-                        <h3 className="text-primary fs-5 mt-3 fw-bold">Leave Details</h3>
-                    </div>
-                    <div className="col-lg-8">
-                    </div>
-                    <div className="col-lg-2">
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-lg-10">
-                    </div>
-                </div>
+            <div>
+        <div className={leaveform.row}>
+          <div className="col-md-12">
+            <div className="row">
+              <div className="col-lg-2">
                 <br />
-                <div className={leaveform.card}>
-                    <form >
-                        <div className="row leavereq">
-                            <div className="col-md-2">
-                                <label className='fw-bold'>Leave Type<span className={leaveform.span}>*</span>
-                                </label></div><div className="col-md-4"><label className='fw-bold'>Leave Description<span className={leaveform.span}>*</span>
-                                </label>
-                            </div>
-                        </div>
-                        <br />
-                        <div className="row leavereq">
-                            <div className="col-md-2">
-                                <input name="LeaveType" type="text" {...register('LeaveType')} placeholder='Leave Type' className={`form-control ${errors.LeaveType ? 'is-invalid' : ''}`} />
-                                <div className="invalid-feedback">{errors.LeaveType?.message}</div>
-                            </div>
-                            <div className="col-md-4">
-                                <textarea name="Description" rows="3" type="text" placeholder='Description' className={`form-control ${errors.Description ? 'is-invalid' : ''}`} />
-                                <div className="invalid-feedback">{errors.Description?.message}</div>
-
-                            </div>
-                        </div>
-                        <br /><br />
-                        <div className="row">
-                            <div className="col-lg-8">
-                            </div>
-                            <div className="col-lg-2">
-                                <Link id={leaveform.link} href="/Masters/LeaveTypeDashboard"> <button className={leaveform.button} tabindex="0">
-                                    Cancel</button></Link>
-                            </div>
-                            <div className="col-lg-2">
-
-                                <button class={leaveform.button} >
-                                    SUBMIT
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-
+                <h3 className="text-primary fs-5 mt-3 fw-bold">
+                  Leave Details
+                </h3>
+              </div>
+              <div className="col-lg-8"></div>
+              <div className="col-lg-2"></div>
             </div>
+            <br />
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className={leaveform.card}>
+                <div className="row">
+                  <div className="col-md-2">
+                    <label className="fw-bold" style={customStyles.inputLabel}>
+                    Leave Type<span style={{ color: "red" }}>*</span>
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Enter Name"
+                      {...register("Short", { required: true })}
+                    ></input>
+                    {errors.Short && (
+                      <span style={customStyles.errorMsg}>
+                        Please Enter Leave Type
+                      </span>
+                    )}
+                  </div>
+
+                  <div className="col-md-4">
+                    <label className="fw-bold" style={customStyles.inputLabel}>
+                      Leave Description<span style={{ color: "red" }}>*</span>
+                    </label>
+                    <textarea
+                      className="form-control"
+                      name="Description"
+                      rows="3"
+                      type="text"
+                      {...register("Description", { required: true })}
+                      placeholder="Description"
+                    />
+                    {errors.Description && (
+                      <span style={customStyles.errorMsg}>
+                        Please Enter Leave Description
+                      </span>
+                    )}
+                  </div>
+                </div>
+              </div>
+
+              <br />
+              <br />
+              <div className="row">
+                <div className="col-lg-7"></div>
+                <div className="col-lg-2">
+                  <Link href="/Masters/subsectionmaster">
+                    <button className={leaveform.button}>
+                      Cancel
+                    </button>
+                  </Link>
+                </div>
+                <div className="col-lg-2">
+                  {actionType == "insert" && (
+                    <button
+                      type="submit"
+                      className={leaveform.button}
+                    >
+                      Save
+                    </button>
+                  )}
+                  {actionType == "update" && (
+                    <button
+                      type="submit"
+                      className={leaveform.button}
+                    >
+                      Update
+                    </button>
+                  )}
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
 
         </Layout>
     )
 }
 
-export default dynamic(() => Promise.resolve(LeaveTypeForm), { ssr: false })
+export default LeaveTypeForm;
--- pages/Masters/leveltypedashboard.js
+++ pages/Masters/leveltypedashboard.js
@@ -2,6 +2,7 @@ import React,{useState,useEffect} from 'react'
 import Link from 'next/link'
 import Layout from '@/Components/layout'
 import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 function LevelTypeDash() {
 

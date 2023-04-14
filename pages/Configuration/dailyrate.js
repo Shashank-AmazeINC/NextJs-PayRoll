d8d4358ae92dff1c3289be6fff36e5769fc966f9
sanath@amazeinc.in <sanath@amazeinc.in>
2023-04-14 10:23:08 +0530

    dailrateadd

--- pages/Configuration/dailyrate.js
+++ pages/Configuration/dailyrate.js
@@ -1,7 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import Styles from '../../styles/dailyrate.module.css'
+import axios from 'axios'
 function dailyrate() {
+
+    const [dailyRate, setDailyRate] = useState([])
+    let hostURL = process.env.NEXT_PUBLIC_API_HOST_URL;
+
+    const getDailyRate = async () => {
+        const { data } = await axios.get(hostURL + "HR/GetDailyrateConfigaration")
+        setDailyRate(data)
+    }
+    const handleDelete = async (id) => {
+        try {
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+
+                    const res = axios.get(hostURL + `HR/DeleteDailyrateConfigaration?ID=${id}`);
+                    console.log(res.data);
+                    // alert("Data deleted successfully");
+                    Swal.fire(
+                        'Deleted!',
+                        'Your file has been deleted.',
+                        'success'
+                    )
+                } getDepartmentMaster();
+
+            })
+
+        } catch (error) {
+            console.error(error);
+            alert("Failed to delete data");
+        }
+    };
+    const getData = (data) => {
+        sessionStorage.setItem("id", data.id);
+        console.log(data.id)
+    }
+
+    const clearFormData = () => {
+        sessionStorage.setItem("id", "");
+    }
+
+
+    useEffect(() => {
+        getDailyRate();
+    }, [])
     return (
         <div>
             <br />
@@ -20,7 +72,7 @@ function dailyrate() {
                 <div className='col-lg-11'></div>
                 <div className='col-lg-1'>
 
-                    <Link href="/configuration/sssconfigadd" id={Styles.addLink} >  <button id={Styles.addButton}>  ADD </button></Link>
+                    <Link href="/Configuration/dailyrateadd" id={Styles.addLink} >  onClick={clearFormData.bind(this)}  <button id={Styles.addButton}>  ADD </button></Link>
 
                 </div>
 
@@ -37,18 +89,22 @@ function dailyrate() {
                         <th>Action</th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody>{
+                    dailyRate.map((data) => {
+                        <tr key={data.id}>
+                            <td>{data.staffid}</td>
+                            <td>{data.Working_Days_Year}</td>
+                            <td>{data.Working_Days_Month}</td>
+                            <td>{data.Working_Hours_Day}</td>
 
-                    <tr>
-                        <td>0</td>
-                        <td>10000</td>
-                        <td>200</td>
-                        <td>2023</td>
+                            <td><button id={Styles.actionButton} className='btn btn-sm' onClick={getData.bind(this, data)}>Edit</button>&nbsp;&nbsp;&nbsp;&nbsp;
+                                <button id={Styles.actionButton} onClick={handleDelete(data.id)} className='btn btn-sm'>Delete</button>&nbsp;&nbsp;&nbsp;&nbsp;
+                            </td>
+                        </tr>
+                    })
+                }
 
-                        <td><button id={Styles.actionButton} className='btn btn-sm'>Edit</button>&nbsp;&nbsp;&nbsp;&nbsp;
-                            <button id={Styles.actionButton} className='btn btn-sm'>Delete</button>&nbsp;&nbsp;&nbsp;&nbsp;
-                        </td>
-                    </tr>
+
                 </tbody>
             </table>
 
